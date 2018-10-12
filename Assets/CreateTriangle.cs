97bb7e7c30e18155c937419821937b1147d339d4
Vico Heidbrink <vico-heidbrink@etud.univ-tln.fr>
2018-10-12 22:59:54 +0200

    Suricate walks and sees!

---
+++ Assets/CreateTriangle.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateTriangle : MonoBehaviour {
+
+    Mesh mesh;
+    Vector3[] vertices;
+    int[] triangles;
+
+	// Use this for initialization
+	void Start () {
+        mesh = new Mesh();
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        vertices = new[] {
+            new Vector3(0,0,0),
+            new Vector3(-4,0,5),
+            new Vector3(4,0,5)
+        };
+
+        mesh.vertices = vertices;
+
+        triangles = new[] { 0, 1, 2 };
+
+        mesh.triangles = triangles;
+
+        GetComponent<MeshCollider>().sharedMesh = mesh;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnCollisionEnter(Collision collision) {
+        Debug.Log("Collided with " + collision.gameObject.name);
+    }
+}
---
+++ Assets/Scripts/WanderBehaviour.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WanderBehaviour : MonoBehaviour {
+
+    public float wanderingRadius;
+    public float wanderingTime;
+    public float moveSpeed;
+    public float rotationSpeed;
+    public float visionDepth;
+    public float visionAngle;
+
+    public GameObject enemy;
+
+    private LineRenderer lineRenderer;
+
+    private NavMeshAgent agent;
+    private Vector3 destination;
+    private Vector3 direction;
+    private Quaternion rotation;
+
+    private float timer;
+
+	// Use this for initialization
+	void Start () {
+        lineRenderer = GetComponent<LineRenderer>();
+        agent = GetComponent<NavMeshAgent>();
+        GetNewDestination();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        timer += Time.deltaTime;
+        //DrawVisionField();
+        //CheckForObject();
+        if (timer >= wanderingTime)
+            GetNewDestination();
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, rotationSpeed * Time.deltaTime);
+        transform.Translate(0, 0, Time.deltaTime * moveSpeed);
+    }
+
+    // UNUSED
+    private void CheckForObject() {
+        float angle = Vector3.Angle(transform.forward, enemy.transform.position);
+        Debug.Log(angle);
+        if (angle < visionAngle/2) {
+            Debug.Log("ENEMY FOUND");
+        }
+    }
+
+    // UNUSED
+    private void DrawVisionField() {
+        if (direction != Vector3.zero) {
+            lineRenderer.SetPosition(0, transform.position);
+            lineRenderer.SetPosition(1, transform.position + transform.forward * visionDepth);
+        }
+    }
+
+    private void GetNewDestination() {
+        destination = RandomDestination(wanderingRadius, -1);
+        direction = destination - transform.position;
+        //agent.SetDestination(destination);
+        rotation = Quaternion.LookRotation(direction);
+        timer = 0;
+    }
+
+    private Vector3 RandomDestination(float distance, int layermask) {
+        Vector3 randomDirection = Random.insideUnitSphere * distance;
+        randomDirection += transform.position;
+        NavMeshHit navHit;
+        NavMesh.SamplePosition(randomDirection, out navHit, distance, layermask);
+        return navHit.position;
+    }
+}
