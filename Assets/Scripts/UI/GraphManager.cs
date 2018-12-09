36d6628dc70d1a7d346a2bdee781b4c1cf28e9a9
Vico Heidbrink <vico-heidbrink@etud.univ-tln.fr>
2018-12-09 12:24:31 +0100

    Finished graph with test values

--- Assets/Scripts/UI/GraphManager.cs
+++ Assets/Scripts/UI/GraphManager.cs
@@ -8,27 +8,34 @@ public class GraphManager : MonoBehaviour {
 	[SerializeField] Sprite dotSprite;
 	[SerializeField] GameObject xAxisLabelGO;
 	[SerializeField] GameObject yAxisLabelGO;
+	[SerializeField] GameObject xAxisDashGO;
+	[SerializeField] GameObject yAxisDashGO;
 
-	private RectTransform container;
+	private RectTransform mainContainer;
+	private RectTransform dataContainer;
 	private RectTransform xAxisLabel;
 	private RectTransform yAxisLabel;
+	private RectTransform xAxisDash;
+	private RectTransform yAxisDash;
 
 	private List<int> values;
 
 	// Use this for initialization
 	void Start () {
-		container = GetComponent<RectTransform>();
+		mainContainer = GetComponent<RectTransform>();
 		xAxisLabel = xAxisLabelGO.GetComponent<RectTransform>();
 		yAxisLabel = yAxisLabelGO.GetComponent<RectTransform>();
+		xAxisDash = xAxisDashGO.GetComponent<RectTransform>();
+		yAxisDash = yAxisDashGO.GetComponent<RectTransform>();
 		values = new List<int>();
 
 		StartCoroutine(AddValues());
 	}
 
 	private IEnumerator AddValues(){
-		for(int i=0; i<40; i++){
-			Debug.Log(i*5);
-			values.Add(i*5);
+		for(int i=0; i<100; i++){
+			Debug.Log((i+1)*10);
+			values.Add((i+1)*10);
 			PopulateGraph(values);
 			yield return new WaitForSeconds(0.01f);
 		}
@@ -36,7 +43,7 @@ public class GraphManager : MonoBehaviour {
 	
 	private GameObject CreatePoint(Vector2 anchoredPosition){
 		GameObject point = new GameObject("Point", typeof(Image));
-		point.transform.SetParent(container, false);
+		point.transform.SetParent(dataContainer, false);
 		point.GetComponent<Image>().sprite = dotSprite;
 		RectTransform rectTransform = point.GetComponent<RectTransform>();
 		rectTransform.anchoredPosition = anchoredPosition;
@@ -49,14 +56,19 @@ public class GraphManager : MonoBehaviour {
 	private void PopulateGraph(List<int> values){
 		// Clear the graph before populating it
 		foreach(Transform child in transform){
-			if(!child.name.Equals("Background") 
-				&& !child.name.Equals(xAxisLabelGO.name)
-				&& !child.name.Equals(yAxisLabelGO.name)){
-					Destroy(child.gameObject);
-				}
+			// Delete the whole data hierarchy
+			if(child.name.Equals("Data"))
+				Destroy(child.gameObject);
 		}
-		float graphHeight = Mathf.Abs(container.sizeDelta.y);
-		float graphWidth = Mathf.Abs(container.sizeDelta.x);
+		// Recreate the data container
+		dataContainer = new GameObject("Data", typeof(RectTransform)).GetComponent<RectTransform>();
+		dataContainer.transform.SetParent(mainContainer, false);
+		dataContainer.anchorMin = new Vector2(0, 0);
+		dataContainer.anchorMax = new Vector2(1, 1);
+		dataContainer.pivot = new Vector2(0, 0);
+
+		float graphHeight = Mathf.Abs(mainContainer.sizeDelta.y);
+		float graphWidth = Mathf.Abs(mainContainer.sizeDelta.x);
 		
 		// Top and bottom margins
 		float xMargin = 5f;
@@ -69,49 +81,66 @@ public class GraphManager : MonoBehaviour {
 		}
 		// Adjust width so it never goes offscreen
 		float xOffset = (graphWidth - xMargin*2) / values.Count;
+		float xPos = 0f;
+		float yPos = 0f;
 		GameObject lastPoint = null;
 		for(int i=0; i<values.Count; i++){
-			float xPos = i * xOffset + xMargin;
-			float yPos = (values[i] / yMax) * (graphHeight - yMargin*2) + yMargin;
+			xPos = i * xOffset + xMargin;
+			yPos = (values[i] / yMax) * (graphHeight - yMargin*2) + yMargin;
 			if(i == values.Count - 1)
-				Debug.Log("y: "+yPos);
+				Debug.Log("y: "+yPos+" for i: "+i);
 			GameObject currentPoint = CreatePoint(new Vector2(xPos, yPos));
 			if(lastPoint != null){
 				LinkDots(lastPoint.GetComponent<RectTransform>().anchoredPosition,
 							currentPoint.GetComponent<RectTransform>().anchoredPosition);
 			}
 			lastPoint = currentPoint;
-			/*
-			RectTransform labelX = Instantiate(xAxisLabel);
-			labelX.SetParent(container, false);
-			labelX.gameObject.SetActive(true);
-			labelX.anchoredPosition = new Vector2(xPos, 0f);
-			labelX.GetComponent<Text>().text = i.ToString();
-			*/
 		}
 
+		// Axis X
+		float firstPosX = xMargin;
+		float lastPosX = xPos;
 		int horizontalSeparatorAmount = 12;
-		for(int i=0; i<horizontalSeparatorAmount; i++){
+		for(int i=0; i<=horizontalSeparatorAmount; i++){
+			float currentPosX = firstPosX + i*(lastPosX - firstPosX)/horizontalSeparatorAmount;
+			// Dashes
+			RectTransform dashX = Instantiate(xAxisDash);
+			dashX.SetParent(dataContainer, false);
+			dashX.gameObject.SetActive(true);
+			dashX.anchoredPosition = new Vector2(currentPosX, 5f);
+
+			// Labels
 			RectTransform labelX = Instantiate(xAxisLabel);
-			labelX.SetParent(container, false);
+			labelX.SetParent(dataContainer, false);
 			labelX.gameObject.SetActive(true);
-			labelX.anchoredPosition = new Vector2(i * 1f / horizontalSeparatorAmount * graphWidth + xMargin, 0f);
-			labelX.GetComponent<Text>().text = Mathf.RoundToInt((values.Count) * i / (horizontalSeparatorAmount-1)).ToString();
+			labelX.anchoredPosition = new Vector2(currentPosX, 0f);
+			labelX.GetComponent<Text>().text = Mathf.RoundToInt((values.Count) * i / (horizontalSeparatorAmount)).ToString();
 		}
 
+		// Axis Y
+		float firstPosY = yMargin;
+		float lastPosY = yPos;
 		int verticalSeparatorAmount = 10;
-		for(int i=0; i<verticalSeparatorAmount; i++){
+		for(int i=0; i<=verticalSeparatorAmount; i++){
+			float currentPosY = firstPosY + i*(lastPosY - firstPosY)/verticalSeparatorAmount;
+			// Dashes
+			RectTransform dashY = Instantiate(yAxisDash);
+			dashY.SetParent(dataContainer, false);
+			dashY.gameObject.SetActive(true);
+			dashY.anchoredPosition = new Vector2(10f, currentPosY);
+
+			// Labels
 			RectTransform labelY = Instantiate(yAxisLabel);
-			labelY.SetParent(container, false);
+			labelY.SetParent(dataContainer, false);
 			labelY.gameObject.SetActive(true);
-			labelY.anchoredPosition = new Vector2(0f, i * 1f / verticalSeparatorAmount * graphHeight + yMargin);
-			labelY.GetComponent<Text>().text = Mathf.RoundToInt((yMax - yMargin) * i / (verticalSeparatorAmount-1)).ToString();
+			labelY.anchoredPosition = new Vector2(0f, currentPosY);
+			labelY.GetComponent<Text>().text = Mathf.RoundToInt((yMax - yMargin) * i / (verticalSeparatorAmount)).ToString();
 		}
 	}
 
 	private void LinkDots(Vector2 dotPosA, Vector2 dotPosB){
 		GameObject line = new GameObject("Line", typeof(Image));
-		line.transform.SetParent(container, false);
+		line.transform.SetParent(dataContainer, false);
 		line.GetComponent<Image>().color = new Color(0f, 0f, 0f);
 		Vector2 direction = (dotPosB - dotPosA).normalized;
 		float distance = Vector2.Distance(dotPosA, dotPosB);
